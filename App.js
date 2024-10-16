76ebed599485d36b11468ddd1f010e6fd206992f
ManasaMajji <36614069+ManasaMajji@users.noreply.github.com>
2024-10-16 21:29:28 +0100

    lesson1 plain react

---
+++ App.js
@@ -0,0 +1,31 @@
+// create this in react
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+    <div id="child2">
+        <h1>I'm h1 tag</h1>
+        <h2>I'm h2 tag</h2>
+    </div>
+</div> */
+}
+
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm a h1 element"),
+    React.createElement("h2", {}, "I'm a h2 element"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm a h1 element"),
+    React.createElement("h2", {}, "I'm a h2 element"),
+  ]),
+]);
+const heading = React.createElement(
+  "h1",
+  { id: "header", xyz: "abc" },
+  "Hello World from React"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
